27e54b686bbe1df0379363f70af0f4f2a460f709
media-admin <markus.tritremmel@media-lab.at>
2022-06-21 19:56:36 +0200

    Set up custom post types

--- cms/wp-content/mu-plugins/parapatits-post-types.php
+++ cms/wp-content/mu-plugins/parapatits-post-types.php
@@ -1,7 +1,6 @@
 <?php
 
-/* --- PARAPATITS CUSTOM POST TYPES --- */
-
+/* === PARAPATITS CUSTOM POST TYPES === */
 function parapatits_post_types() {
 
 	add_post_type_support( 'team', 'thumbnail' );
@@ -13,20 +12,160 @@ function parapatits_post_types() {
 	} );
 
 
-	/* Custom Post Type "HINWEISE" */
+	/* --- Custom Post Type PROJEKTE --- */
+	$labels = array(
+		'name' =>  'Projekte',
+		'add_new' => 'Neues Projekt erstellen',
+		'edit_item' => 'Projekt bearbeiten',
+		'singular_name' => 'Projekt',
+		'all_items' => 'Alle Projekte',
+		'supports' => array('title', 'editor', 'author', 'custom-fields',
+	));
+
+	register_post_type( 'project', array(
+		'show_in_rest' => true,
+		'public' => true,
+		'show_ui' => true,
+		'labels' => $labels,
+		'has_archive' => false,
+		'exclude_from_search' => false,
+		'menu_position' => 10,
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => false,
+		'publicly_queryable'  => true,
+		'menu_icon' => 'dashicons-hammer'
+	));
+
+	add_post_type_support( 'project', 'thumbnail' );
+
+
+	/* --- Custom Post Type FAQ --- */
+	$labels = array(
+		'name' =>  'FAQs',
+		'add_new' => 'Neue FAQ erstellen',
+		'edit_item' => 'FAQ bearbeiten',
+		'singular_name' => 'FAQ',
+		'all_items' => 'Alle FAQs',
+		'supports' => array('title', 'editor', 'author', 'custom-fields',
+	));
+
+	register_post_type( 'faq', array(
+		'show_in_rest' => true,
+		'public' => true,
+		'show_ui' => true,
+		'taxonomies' => array( 'faq-category' ),
+		'labels' => $labels,
+		'has_archive' => false,
+		'exclude_from_search' => false,
+		'rewrite' => array('slug' => 'faq', 'with_front' => true, 'pages' => true, 'feeds' => true,),
+		'menu_position' => 11,
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'publicly_queryable'  => true,
+		'menu_icon' => 'dashicons-lightbulb'
+	));
+
+
+	/* --- Custom Post Type TESTIMONIALS --- */
+	$labels = array(
+		'name' =>  'Bewertungen',
+		'add_new' => 'Neue Bewertung einstellen',
+		'edit_item' => 'Bewertung bearbeiten',
+		'singular_name' => 'Bewertung',
+		'all_items' => 'Alle Bewertungen',
+		'supports' => array('title', 'editor', 'author', 'custom-fields'
+	));
+
+	register_post_type( 'testimonial', array(
+		'show_in_rest' => true,
+		'public' => true,
+		'show_ui' => true,
+		'taxonomies' => array( 'testimonial-category' ),
+		'labels' => $labels,
+		'has_archive' => false,
+		'exclude_from_search' => false,
+		'rewrite' => array('slug' => 'testimonial'),
+		'menu_position' => 12,
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'publicly_queryable'  => true,
+		'menu_icon' => 'dashicons-format-status'
+	));
+
+	add_post_type_support( 'testimonial', 'thumbnail' );
+
+
+	/* --- Custom Post Type JOBS --- */
+	$labels = array(
+		'name' =>  'Jobs',
+		'add_new' => 'Neuen Job einstellen',
+		'edit_item' => 'Job bearbeiten',
+		'singular_name' => 'Job',
+		'all_items' => 'Alle Jobs',
+		'supports' => array('title', 'editor', 'author', 'custom-fields'
+	));
+
+	register_post_type( 'job', array(
+		'show_in_rest' => true,
+		'public' => true,
+		'show_ui' => true,
+		'labels' => $labels,
+		'has_archive' => false,
+		'exclude_from_search' => false,
+		'rewrite' => array('slug' => 'jobs'),
+		'menu_position' => 13,
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'publicly_queryable'  => true,
+		'menu_icon' => 'dashicons-nametag'
+	));
+
+	add_post_type_support( 'job', 'thumbnail' );
+
+
+	/* --- Custom Post Type PARTNER --- */
+	$labels = array(
+		'name' =>  'Partner',
+		'add_new' => 'Neuen Partner anlegen',
+		'edit_item' => 'Partner bearbeiten',
+		'singular_name' => 'Partner',
+		'all_items' => 'Alle Partner',
+		'supports' => array('title', 'editor', 'author', 'custom-fields'
+	));
+
+	register_post_type( 'partner', array(
+		'show_in_rest' => true,
+		'public' => true,
+		'show_ui' => true,
+		'taxonomies' => array( 'partner-category' ),
+		'labels' => $labels,
+		'has_archive' => false,
+		'exclude_from_search' => false,
+		'menu_position' => 14,
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'publicly_queryable'  => true,
+		'menu_icon' => 'dashicons-networking'
+	));
+
+	add_post_type_support( 'partner', 'thumbnail' );
+
+
+	/* --- Custom Post Type HINWEISE ---
+	$labels = array(
+		'name' =>  'Hinweise',
+		'add_new' => 'Neuen Hinweis erstellen',
+		'edit_item' => 'Hinweis bearbeiten',
+		'singular_name' => 'Hinweis',
+		'all_items' => 'Alle Hinweise',
+		'supports' => array('title', 'editor', 'author', 'custom-fields'
+	));
 
 	register_post_type( 'notification', array(
 		'show_in_rest' => true,
 		'public' => true,
 		'show_ui' => true,
-		'labels' => array(
-			'name' =>  'Hinweise',
-			'add_new' => 'Neuen Hinweis erstellen',
-			'edit_item' => 'Hinweis bearbeiten',
-			'singular_name' => 'Hinweis',
-			'all_items' => 'Alle Hinweise',
-			'supports' => array('title', 'editor', 'author', 'custom-fields', ),
-		),
+		'labels' => $labels,
 		'has_archive' => false,
 		'exclude_from_search' => false,
 		'rewrite' => array('slug' => 'hinweise'),
@@ -38,25 +177,23 @@ function parapatits_post_types() {
 	));
 
 
-	/* Custom Post Type "SLIDER" */
-
-	add_post_type_support( 'slider', 'thumbnail' );
+	/* --- Custom Post Type SLIDER ---
+	$labels = array(
+		'name' =>  'Header Slider',
+		'add_new' => 'Neues Slider-Bild erstellen',
+		'edit_item' => 'Slider-Bild bearbeiten',
+		'singular_name' => 'Slider',
+		'all_items' => 'Alle Slider',
+		'supports' => array('title', 'editor', 'author', 'custom-fields'
+	));
 
 	register_post_type( 'slider', array(
 		'show_in_rest' => true,
 		'public' => true,
 		'show_ui' => true,
-		'labels' => array(
-			'name' =>  'Header Slider',
-			'add_new' => 'Neues Slider-Bild erstellen',
-			'edit_item' => 'Slider-Bild bearbeiten',
-			'singular_name' => 'Slider',
-			'all_items' => 'Alle Slider',
-			'supports' => array('title', 'editor', 'author', 'custom-fields', ),
-		),
+		'labels' => $labels,
 		'has_archive' => false,
-		'exclude_from_search' => false,
-		'rewrite' => array('slug' => 'sliders'),
+		'exclude_from_search' => true,
 		'menu_position' => 7,
 		'show_in_admin_bar'   => false,
 		'show_in_nav_menus'   => false,
@@ -64,125 +201,118 @@ function parapatits_post_types() {
 		'menu_icon' => 'dashicons-superhero'
 	));
 
+	add_post_type_support( 'slider', 'thumbnail' );
 
-	/* Custom Post Type "TESTIMONIALS" */
 
-	add_post_type_support( 'testimonial', 'thumbnail' );
+	/* --- Custom Post Type COMPANY-DETAILS --- */
+	$labels = array(
+		'name' =>  'Firmendetails',
+		'supports' => array('title', 'editor', 'author', 'custom-fields'
+	));
 
-	register_post_type( 'testimonial', array(
+	register_post_type( 'company-details', array(
 		'show_in_rest' => true,
 		'public' => true,
 		'show_ui' => true,
-		'labels' => array(
-			'name' =>  'Testimonials',
-			'add_new' => 'Neues Testimonal erstellen',
-			'edit_item' => 'Testimonial bearbeiten',
-			'singular_name' => 'Testimonial',
-			'all_items' => 'Alle Testimonials',
-			'supports' => array('title', 'editor', 'author', 'custom-fields', ),
-		),
+		'labels' => $labels,
 		'has_archive' => false,
 		'exclude_from_search' => false,
-		'rewrite' => array('slug' => 'testimonial'),
-		'menu_position' => 8,
+		'menu_position' => 15,
 		'show_in_admin_bar'   => false,
 		'show_in_nav_menus'   => false,
 		'publicly_queryable'  => true,
-		'menu_icon' => 'dashicons-format-quote'
+		'menu_icon' => 'dashicons-building'
 	));
 
+	add_post_type_support( 'company-details', 'thumbnail' );
 
 
-	/* Custom Post Type "PRODUKTE" */
 
-	add_post_type_support( 'products', 'thumbnail' );
-
-	register_post_type( 'products', array(
-		'show_in_rest' => true,
-		'public' => true,
-		'show_ui' => true,
-		'taxonomies' => array( 'product-category' ),
-		'labels' => array(
-			'name' =>  'Produkt-Arten',
-			'add_new' => 'Neue Produkt-Art hinzufügen',
-			'edit_item' => 'Produkt-Art bearbeiten',
-			'singular_name' => 'Produkt-Art',
-			'all_items' => 'Alle Produkt-Arten',
-			'supports' => array('title', 'editor', 'author', 'custom-fields', ),
-		),
-		'has_archive' => true,
-		'exclude_from_search' => false,
-		// 'rewrite' => array('slug' => 'produkte', 'with_front' => true, 'pages' => true, 'feeds' => true,),
-		'menu_position' => 6,
-		'show_in_admin_bar'   => false,
-		'show_in_nav_menus'   => false,
-		'publicly_queryable'  => true,
-		'menu_icon' => 'dashicons-editor-table'
-	));
-
-
-	add_post_type_support( 'team-members', 'thumbnail' );
-
-	register_post_type( 'team-members', array(
-		'show_in_rest' => true,
-		'public' => true,
-		'show_ui' => true,
-		'labels' => array(
-			'name' =>  'Team-Mitglieder',
-			'add_new' => 'Neues Team-Mitglied hinzufügen',
-			'edit_item' => 'Team-Mitglied bearbeiten',
-			'singular_name' => 'Team-Mitglied',
-			'all_items' => 'Alle Team-Mitglieder',
-			'supports' => array('title', 'editor', 'author', 'custom-fields', ),
-		),
-		'has_archive' => false,
-		'exclude_from_search' => false,
-		'rewrite' => array('slug' => 'team-members'),
-		'menu_position' => 7,
-		'show_in_admin_bar'   => false,
-		'show_in_nav_menus'   => false,
-		'publicly_queryable'  => true,
-		'menu_icon' => 'dashicons-id'
-	));
 
 }
 
 
 
-/* --- PARAPATITS CUSTOM TAXONOMIES --- */
+/* === PARAPATITS CUSTOM TAXONOMIES === */
 
 function parapatits_taxonomies() {
 
-	/* Custom Taxonomie "PRODUKT-KATEGORIE" */
-
-	 $labels = array(
-		'name' => _x( 'Produkt-Kategorien', 'taxonomy general name' ),
-		'singular_name' => _x( 'Produkt-Kategorie', 'taxonomy singular name' ),
-		'search_items' =>  __( 'Produkt-Kategorien durchsuchen' ),
-		'popular_items' => __( 'Meist benutzte Produkt-Kategorien' ),
-		'all_items' => __( 'Alle Produkt-Kategorien' ),
-		'parent_item' => __( 'Übergeordnete Produkt-Kategorie' ),
-		'parent_item_colon' => __( 'Übergeordnete Produkt-Kategorien:' ),
-		'edit_item' => __( 'Produkt-Kategorie bearbeiten' ),
-		'update_item' => __( 'Produkt-Kategorien aktualisieren' ),
-		'add_new_item' => __( 'Neue Produkt-Kategorie hinzufügen' ),
-		'new_item_name' => __( 'Name der neuen Produkt-Kategorie' ),
+	/* --- Custom Taxonomie FAQ-KATEGORIE --- */
+	$labels = array(
+		'name' => _x( 'FAQ-Kategorien', 'taxonomy general name' ),
+		'singular_name' => _x( 'FAQ-Kategorie', 'taxonomy singular name' ),
+		'search_items' =>  __( 'FAQ-Kategorien durchsuchen' ),
+		'popular_items' => __( 'Meist benutzte FAQ-Kategorien' ),
+		'all_items' => __( 'Alle FAQ-Kategorien' ),
+		'parent_item' => __( 'Übergeordnete FAQ-Kategorie' ),
+		'parent_item_colon' => __( 'Übergeordnete FAQ-Kategorien:' ),
+		'edit_item' => __( 'FAQ-Kategorie bearbeiten' ),
+		'update_item' => __( 'FAQ-Kategorie aktualisieren' ),
+		'add_new_item' => __( 'Neue FAQ-Kategorien hinzufügen' ),
+		'new_item_name' => __( 'Name der neuen FAQ-Kategorien' ),
 	);
 
-
-	register_taxonomy('product-category', array('products'), array(
+	register_taxonomy('faq-category', array('faqs'), array(
 		'hierarchical' => true,
 		'labels' => $labels,
 		'show_ui' => true,
 		'show_in_rest' => true,
 		'query_var' => true,
-		// 'rewrite' => array( 'slug' => 'produkte' ),
+		'rewrite' => array( 'slug' => 'faqs' ),
+	));
+
+
+	/* --- Custom Taxonomie TESTIMONIAL-KATEGORIE --- */
+	$labels = array(
+		'name' => _x( 'Bewertungs-Kategorien', 'taxonomy general name' ),
+		'singular_name' => _x( 'Bewertungs-Kategorie', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Bewertungs-Kategorien durchsuchen' ),
+		'popular_items' => __( 'Meist benutzte Bewertungs-Kategorien' ),
+		'all_items' => __( 'Alle Bewertungs-Kategorien' ),
+		'parent_item' => __( 'Übergeordnete Bewertungs-Kategorie' ),
+		'parent_item_colon' => __( 'Übergeordnete Bewertungs-Kategorien:' ),
+		'edit_item' => __( 'Bewertungs-Kategorie bearbeiten' ),
+		'update_item' => __( 'Bewertungs-Kategorie aktualisieren' ),
+		'add_new_item' => __( 'Neue Bewertungs-Kategorien hinzufügen' ),
+		'new_item_name' => __( 'Name der neuen Bewertungs-Kategorie' ),
+	);
+
+	register_taxonomy('testimonial-category', array('testimonials'), array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_in_rest' => true,
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'testimonials' ),
+	));
+
+
+	/* --- Custom Taxonomie PARTNER-KATEGORIE --- */
+	$labels = array(
+		'name' => _x( 'Partner-Kategorien', 'taxonomy general name' ),
+		'singular_name' => _x( 'Partner-Kategorie', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Partner-Kategorien durchsuchen' ),
+		'popular_items' => __( 'Meist benutzte Partner-Kategorien' ),
+		'all_items' => __( 'Alle Partner-Kategorien' ),
+		'parent_item' => __( 'Übergeordnete Partner-Kategorie' ),
+		'parent_item_colon' => __( 'Übergeordnete Partner-Kategorien:' ),
+		'edit_item' => __( 'Partner-Kategorie bearbeiten' ),
+		'update_item' => __( 'Partner-Kategorie aktualisieren' ),
+		'add_new_item' => __( 'Neue Partner-Kategorien hinzufügen' ),
+		'new_item_name' => __( 'Name der neuen Partner-Kategorie' ),
+	);
+
+	register_taxonomy('partner-category', array('partners'), array(
+		'hierarchical' => false,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_in_rest' => true,
+		'query_var' => true,
 	));
 
 }
 
 add_action('init', 'parapatits_post_types');
-
 add_action( 'init', 'parapatits_taxonomies', 0 );
 
 ?>
