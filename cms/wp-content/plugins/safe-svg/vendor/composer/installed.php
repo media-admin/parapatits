4543951359de6f17dcfba8ebb521978ac5d38c7b
media-admin <markus.tritremmel@media-lab.at>
2022-07-21 02:05:22 +0200

    Added plugin safe-svg

---
+++ cms/wp-content/plugins/safe-svg/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'darylldoyle/safe-svg',
+        'pretty_version' => '2.0.2',
+        'version' => '2.0.2.0',
+        'reference' => 'c603e4b5592ac46c71c846d6e81924f1a9f0a761',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'darylldoyle/safe-svg' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => 'c603e4b5592ac46c71c846d6e81924f1a9f0a761',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.15.4',
+            'version' => '0.15.4.0',
+            'reference' => 'e50b83a2f1f296ca61394fe88fbfe3e896a84cf4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
