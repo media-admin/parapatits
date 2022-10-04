39b72bc999f98d555015097d19d1742f1d4626d9
media-admin <markus.tritremmel@media-lab.at>
2022-10-04 11:37:42 +0000

    Updated WordPress plugins and finished Jobs page

--- cms/wp-content/themes/parapatits/page-jobs.php
+++ cms/wp-content/themes/parapatits/page-jobs.php
@@ -17,42 +17,56 @@ get_header();
 		<img class="img--centered lazyload" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/jobs/parapatits-tischlerei_komp-32-DSC03424_web.jpg" alt="Platzhalter-Bild">
 
 		<section class="box--left-aligned">
-			<article class="wrapper">
-				<h2 class="h2__heading">Offene Stellen</h2>
-				<div class="">
-					<h2 class="h2__heading">PLATZHALTER-TEXT</h2>
-					<p class="">
-						Zur Zeit sind leider keine offenen Stellen vorhanden. Wir freuen uns aber trotzdem auf Ihre Initiativbewerbung.
-					</p>
-					<a class="btn btn--red" role="button" href="mailto:bewerbung@parapatits.at?subject='Bewerbung über die Website'">Bewerbung mailen</a>
-				</div>
-			</article>
+			<h2 class="h2__heading wrapper">Offene Stellen</h2>
 
-		<section class="box--left-aligned">
-			<article class="wrapper">
-				<div class="">
-					<h2 class="h2__heading">Tischlereigeselle/in</h2>
-					<p class="">
-						Auf dich wartet:
-						- ein kollegiales und familiäres Umfeld
-						- ein Junges und motiviertes Team
-						- ein sicherer langfristiger Arbeitsplatz
-						- selbstständige Projektabwicklung von Anfang bis Ende
-					</p>
-					<a class="btn btn--red" role="button" href="/jobs">Jobdetails anzeigen</a>
-				</div>
-			</article>
+			<?php
 
-			<article class="wrapper">
-				<div class="">
-					<h2 class="h2__heading">Planer/Zeichner</h2>
-					<p class="">
-						Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-					</p>
-					<a class="btn btn--red" role="button" href="/jobs">Jobdetails anzeigen</a>
-				</div>
-			</article>
-		</section>
+			$args = array(
+				'post_status' => 'publish',
+				'posts_per_page' => 1,
+				'post_type' => 'job',
+			);
+
+			$loop = new WP_Query( $args );
+
+			if ($loop->have_posts() ) {
+
+				while ( $loop->have_posts() ) : $loop->the_post();
+
+					global $post;
+					$content = apply_filters('the_content', wpautop($post->post_content)); ?>
+
+					<article class="job__container wrapper">
+						<div class="job__image">
+							<?php the_post_thumbnail('full', ['class' => '']); ?>
+						</div>
+						<div class="job__description wrapper">
+							<h3 class="job__title h3__heading"><?php the_title();?></h3> <!-- TODO 'Check if h3 format is correct -->
+							<?php echo $content; ?>
+							<a class="job__application-button btn btn--red" role="button" href="mailto:bewerbung@parapatits.at?subject='Bewerbung über die Website'">Bewerbung mailen</a>
+						</div>
+					</article>
+
+				<?php endwhile; ?>
+
+				<?php
+				wp_reset_postdata();
+
+			} else { ?>
+				<article class="job__container wrapper">
+					<div class="job__image">
+
+					</div>
+					<div class="job__description wrapper">
+						<h3 class="job__title h3__heading">Aktuell ist unser Team komplett</h3>
+						<p class="">
+							Zur Zeit sind leider keine offenen Stellen vorhanden. Wir freuen uns aber trotzdem auf Ihre Initiativbewerbung.
+						</p>
+						<a class="btn btn--red" role="button" href="mailto:bewerbung@parapatits.at?subject='Bewerbung über die Website'">Bewerbung mailen</a>
+					</div>
+				</article>
+			<?php
+			} ?>
 
 		<img class="img--fullwidth lazyload" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/jobs/parapatits-tischlerei_komp-48-DSC03529_web.jpg" alt="Platzhalter-Bild">
 
