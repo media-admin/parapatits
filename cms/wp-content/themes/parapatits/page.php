989eeeac5a15f12a42ab76c4bcb4870f5951d671
media-admin <markus.tritremmel@media-lab.at>
2022-10-10 22:24:31 +0200

    Styled default page and list tags

--- cms/wp-content/themes/parapatits/page.php
+++ cms/wp-content/themes/parapatits/page.php
@@ -2,19 +2,24 @@
 /**
 * Template Name: Seite Standard
 */
-
 get_header();
 ?>
-
-<main class="site-main">
-	<div class="site-content">
-		<h1 class="site-title h1__title">
-			<?php the_title();?>
-		</h1>
-		<?php
-			the_content();
-		?>
-	</div>
-</main>
-
+	<main class="site-main">
+		<div class="site-content">
+			<section class="site-intro box--left-aligned">
+				<article class="wrapper">
+					<h1 class="site-title h1__title">
+						<?php the_title();?>
+					</h1>
+					<div class="page__content-container wrapper">
+						<?php
+							global $post;
+							$content = apply_filters('the_content',$post->post_content);
+							echo $content;
+						?>
+					</div>
+					</article>
+			</section>
+		</div>
+	</main>
 <?php get_footer(); ?>
